faa18099a65cc77c70b54336ca0dd8d574d1a736
benfminer <benfminer@gmail.com>
2024-04-07 14:30:18 -0700

    Completed the substrings Project

--- substrings.rb
+++ substrings.rb
@@ -2,15 +2,20 @@
 # word. Then create a hash with the substrings and thier counts
 
 def substrings(string, dictionary)
-  subs = {}
-  dictionary.each do |word|
-    if string.include?(word)
-     subs = {word.to_s => string.scan(word).length}
+  sub_hash = {} # Create empty hash
+
+  dictionary.each do |dict_word|  # Loop through dictionary
+    count = 0 # create count variable
+
+    string.split(" ").each do |sub_word|  # Loop through string
+      strip_sub_word = sub_word.gsub(/[[:punct:]]/, "") #create new variable to with word stripped of punctuation
+      if strip_sub_word.downcase.include?(dict_word)  #Check if string word is in dictionary word, if so increase count
+        count += 1
+      end
+    end
+    if count > 0  # If string word found in dictionary then create hash entry with count of number of times string included
+      sub_hash[dict_word] = count
     end
   end
-  puts subs
+  return sub_hash
 end
-
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-substrings("below", dictionary)
